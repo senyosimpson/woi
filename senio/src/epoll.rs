cca6f5937be7fc73b2649b21b5c9ecc99e13c8fb
SenYeezus <simpsonsenyo@gmail.com>
2021-05-28 00:47:46 +0200

    feat: front facing API for senio done

--- senio/src/epoll.rs
+++ senio/src/epoll.rs
@@ -1,12 +1,12 @@
 use std::io;
 use std::os::unix::io::RawFd;
 
-use libc;
 use bitflags::bitflags;
+use libc;
 
 /// Control options for `epoll_ctl`
 #[repr(i32)]
-pub enum CtlOp {
+pub(crate) enum CtlOp {
     /// Adds an entry to the interest list
     ADD = libc::EPOLL_CTL_ADD,
     /// Change the settings of an associated entry in the interest list
@@ -15,37 +15,48 @@ pub enum CtlOp {
     DEL = libc::EPOLL_CTL_DEL,
 }
 
-/// An equivalent of `libc::epoll_create`
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct Token(pub u64);
+
+/// An equivalent of `libc::epoll_data`
 #[repr(C)]
 #[repr(packed)]
 #[derive(Debug, Clone, Copy)]
 pub struct Event {
-    events: u32,
+    interest: u32,
     data: u64,
 }
 
 impl Event {
-    pub fn new(events: Events, data: u64) -> Event {
+    pub fn new(interest: Interest, token: Token) -> Event {
         Event {
-            events: events.bits(),
-            data: data
+            interest: interest.bits(),
+            data: token.0,
+        }
+    }
+
+    pub fn empty() -> Event {
+        Event {
+            interest: 0,
+            data: 0,
         }
     }
 }
 
+pub type Events = Vec<Event>;
+
 // TODO: Write documentation
 bitflags! {
-    pub struct Events: u32 {
-        const EPOLLIN        = libc::EPOLLIN as u32;
-        const EPOLLOUT       = libc::EPOLLOUT as u32;
-        const EPOLLRDHUP     = libc::EPOLLRDHUP as u32;
-        const EPOLLPRI       = libc::EPOLLPRI as u32;
-        const EPOLLERR       = libc::EPOLLERR as u32;
-        const EPOLLHUP       = libc::EPOLLHUP as u32;
-        const EPOLLET        = libc::EPOLLET as u32;
-        const EPOLLONESHOT   = libc::EPOLLONESHOT as u32;
-        const EPOLLWAKEUP    = libc::EPOLLWAKEUP as u32;
-        const EPOLLEXCLUSIVE = libc::EPOLLEXCLUSIVE as u32;
+    pub struct Interest: u32 {
+        const READABLE       = (libc::EPOLLIN  | libc::EPOLLONESHOT) as u32;
+        const WRITABLE       = (libc::EPOLLOUT | libc::EPOLLONESHOT) as u32;
+        // const EPOLLRDHUP     = libc::EPOLLRDHUP as u32;
+        // const EPOLLPRI       = libc::EPOLLPRI as u32;
+        // const EPOLLERR       = libc::EPOLLERR as u32;
+        // const EPOLLHUP       = libc::EPOLLHUP as u32;
+        // const EPOLLET        = libc::EPOLLET as u32;
+        // const EPOLLWAKEUP    = libc::EPOLLWAKEUP as u32;
+        // const EPOLLEXCLUSIVE = libc::EPOLLEXCLUSIVE as u32;
     }
 }
 
@@ -58,25 +69,24 @@ fn cvt(result: i32) -> io::Result<i32> {
     }
 }
 
-
 /// Safe wrapper around `libc::epoll_create`
 /// Manpages: https://man7.org/linux/man-pages/man2/epoll_create.2.html
 #[cfg(target_os = "linux")]
-pub fn create(size: i32) -> io::Result<RawFd> {
+pub(crate) fn create(size: i32) -> io::Result<RawFd> {
     cvt(unsafe { libc::epoll_create(size) })
 }
 
 /// Safe wrapper around `libc::epoll_create1`
 /// Manpages: https://man7.org/linux/man-pages/man2/epoll_create1.2.html
 #[cfg(target_os = "linux")]
-pub fn create1(flags: i32) -> io::Result<RawFd> {
+pub(crate) fn create1(flags: i32) -> io::Result<RawFd> {
     cvt(unsafe { libc::epoll_create1(flags) })
 }
 
 /// Safe wrapper around `libc::epoll_ctl`
 /// Manpages: https://man7.org/linux/man-pages/man2/epoll_ctl.2.html
 #[cfg(target_os = "linux")]
-pub fn ctl(epfd: RawFd, op: CtlOp, fd: RawFd, event: &mut Event) -> io::Result<()> {
+pub(crate) fn ctl(epfd: RawFd, op: CtlOp, fd: RawFd, event: &mut Event) -> io::Result<()> {
     let event_ptr: *mut Event = event;
     cvt(unsafe { libc::epoll_ctl(epfd, op as i32, fd, event_ptr as *mut libc::epoll_event) })?;
     Ok(())
@@ -85,15 +95,15 @@ pub fn ctl(epfd: RawFd, op: CtlOp, fd: RawFd, event: &mut Event) -> io::Result<(
 /// Safe wrapper around `libc::epoll_wait`
 /// Manpages: https://man7.org/linux/man-pages/man2/epoll_wait.2.html
 #[cfg(target_os = "linux")]
-pub fn wait(epfd: RawFd, events: &mut [Event], maxevents: i32, timeout: i32) -> io::Result<i32> {
+pub(crate) fn wait(epfd: RawFd, events: &mut Events, timeout: i32) -> io::Result<i32> {
     let events_ptr = events.as_mut_ptr() as *mut libc::epoll_event;
-    cvt(unsafe { libc::epoll_wait(epfd, events_ptr, maxevents, timeout) })
+    cvt(unsafe { libc::epoll_wait(epfd, events_ptr, events.capacity() as i32, timeout) })
 }
 
 /// Safe wrapper around `libc::close`
 /// Manpages: https://man7.org/linux/man-pages/man2/close.2.html
 #[cfg(target_os = "linux")]
-pub fn close(fd: RawFd) -> io::Result<()> {
+pub(crate) fn close(fd: RawFd) -> io::Result<()> {
     cvt(unsafe { libc::close(fd) })?;
     Ok(())
 }
@@ -115,8 +125,8 @@ mod tests {
         use std::os::unix::io::AsRawFd;
 
         let queue = create(1).unwrap();
-        let events = Events::EPOLLIN | Events::EPOLLONESHOT;
-        let mut event = Event::new(events, 1);
+        let interest = Interest::READABLE | Interest::WRITABLE;
+        let mut event = Event::new(interest, Token(1));
 
         let socket = TcpStream::connect("localhost:3000").unwrap();
         ctl(queue, CtlOp::ADD, socket.as_raw_fd(), &mut event).unwrap();
@@ -130,12 +140,11 @@ mod tests {
         use std::os::unix::io::AsRawFd;
 
         let queue = create(1).unwrap();
-        let events = Events::EPOLLIN | Events::EPOLLONESHOT;
-        let mut event = Event::new(events, 1);
+        let interest = Interest::READABLE;
+        let mut event = Event::new(interest, Token(1));
 
-
-        // I need an actual way of testing this without spinning up an entire server. I can potentially
-        // query an actual website.
+        // I need an actual way of testing this without spinning up an entire server.
+        // I can potentially query an actual website.
         let mut socket = TcpStream::connect("localhost:3000").unwrap();
         let request = "GET /delay HTTP/1.1\r\nHost: localhost:3000\r\nConnection: close\r\n\r\n";
         socket.write_all(request.as_bytes()).unwrap();
@@ -143,8 +152,8 @@ mod tests {
         ctl(queue, CtlOp::ADD, socket.as_raw_fd(), &mut event).unwrap();
 
         let maxevents = 10;
-        let mut events: Vec<Event> = Vec::with_capacity(maxevents);
-        let num_events = wait(queue, &mut events, maxevents as i32, -1).unwrap();
+        let mut events = Events::with_capacity(maxevents);
+        let num_events = wait(queue, &mut events, -1).unwrap();
         println!("Received {} number of events!", num_events);
         close(queue).unwrap();
 
--- senio/src/lib.rs
+++ senio/src/lib.rs
@@ -1,32 +1,72 @@
-use epoll;
-use std::{io, os::unix::io::RawFd};
+use std::{
+    io,
+    os::unix::io::{AsRawFd, RawFd},
+    time::Duration,
+};
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct Token(u64);
+mod epoll;
+use epoll::{CtlOp, Event, Events, Interest, Token};
 
-pub struct Registry;
+pub trait Source {
+    fn raw_fd(&self) -> RawFd;
+}
 
-impl Registry {
-    pub fn new() -> Registry {}
+impl Source for RawFd {
+    fn raw_fd(&self) -> RawFd {
+        *self
+    }
+}
+
+impl<T: AsRawFd> Source for &T {
+    fn raw_fd(&self) -> RawFd {
+        self.as_raw_fd()
+    }
 }
 
 pub struct Poll {
     fd: RawFd,
-    registry: Registry
 }
 
 impl Poll {
-    pub fn new() {}
-    pub fn register() {}
-    pub fn deregister() {}
-    pub fn reregister() {}
-    pub fn poll() {}
-}
+    pub fn new() -> io::Result<Poll> {
+        let fd = epoll::create(1)?;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let poll = Poll { fd };
+        Ok(poll)
+    }
+
+    pub fn add(&self, source: impl Source, interest: Interest, token: Token) -> io::Result<()> {
+        let mut event = Event::new(interest, token);
+        epoll::ctl(self.fd, CtlOp::ADD, source.raw_fd(), &mut event)?;
+        Ok(())
+    }
+
+    pub fn delete(&self, source: impl Source) -> io::Result<()> {
+        // Create an event to pass in but it is ignored
+        // NOTE: Because it is ignored, we can pass in a token that's
+        // already in use
+        let mut event = Event::empty();
+        epoll::ctl(self.fd, CtlOp::DEL, source.raw_fd(), &mut event)?;
+        Ok(())
+    }
+    pub fn modify(&self, source: impl Source, interest: Interest, token: Token) -> io::Result<()> {
+        let mut event = Event::new(interest, token);
+        epoll::ctl(self.fd, CtlOp::MOD, source.raw_fd(), &mut event)?;
+        Ok(())
+    }
+
+    pub fn poll(&self, events: &mut Events, timeout: Option<Duration>) -> io::Result<()> {
+        let timeout = match timeout {
+            Some(duration) => duration.as_millis() as i32,
+            None => -1,
+        };
+        epoll::wait(self.fd, events, timeout)?;
+        Ok(())
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn add_event() {}
+// }
