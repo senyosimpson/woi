120b97ae6dccae57d0ce366ac016fc9c8e6d8ce0
SenYeezus <simpsonsenyo@gmail.com>
2021-08-10 22:35:42 +0200

    feat: converted to a workspace so I can keep a reference application

--- woi/src/lib.rs
+++ woi/src/lib.rs
@@ -2,6 +2,7 @@ pub mod epoll;
 pub mod future;
 pub mod io;
 pub mod net;
+
 pub mod task;
 
 pub mod runtime;
--- woi/src/net/tcp.rs
+++ woi/src/net/tcp.rs
@@ -1,31 +1,34 @@
 // TcpListener, TcpStream, Incoming
-use std::io;
+use std::{
+    io::{self, Read},
+    pin::Pin,
+    task::{Context, Poll},
+};
 
-use crate::io::AsyncRead;
 use super::addr::ToSocketAddrs;
+use crate::io::AsyncRead;
 
 pub struct TcpListener {
     inner: std::net::TcpListener,
 }
 
 pub struct TcpStream {
-    inner: std::net::TcpStream
+    inner: std::net::TcpStream,
 }
 
 impl TcpListener {
     pub fn new(inner: std::net::TcpListener) -> io::Result<TcpListener> {
-        // Set to nonblocking
         inner.set_nonblocking(true)?;
         Ok(TcpListener { inner })
     }
 
     pub async fn bind<A: ToSocketAddrs>(addr: A) -> io::Result<TcpListener> {
-        let mut err  = None;
+        let mut err = None;
 
         for addr in addr.to_socket_addrs().await? {
             match std::net::TcpListener::bind(addr) {
                 Ok(listener) => return TcpListener::new(listener),
-                Err(e) => err = Some(e)
+                Err(e) => err = Some(e),
             }
         }
 
@@ -38,10 +41,10 @@ impl TcpListener {
     }
 
     // pub async fn accept<A: ToSocketAddrs>(addr: A) -> io::Result<(net::TcpStream, SocketAddr)> {
-        // let listener = TcpListener::bind(addr).await?;
-        // // match listener.inner.accept() {
+    // let listener = TcpListener::bind(addr).await?;
+    // // match listener.inner.accept() {
 
-        // // }
+    // // }
     // }
 
     pub fn local_addr() {}
@@ -54,5 +57,12 @@ impl TcpListener {
 impl TcpStream {}
 
 impl AsyncRead for TcpStream {
-    
+    fn poll_read(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<io::Result<usize>> {
+        match self.inner.read(buf) {
+            Ok(n) => return Poll::Ready(Ok(n)),
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => Poll::Pending,
+            Err(e) => return Poll::Ready(Err(e)) 
+        }
+        
+    }
 }
