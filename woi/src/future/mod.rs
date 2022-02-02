eab30d3a630ef834bf4b13f79e5bedf972c348f0
SenYeezus <simpsonsenyo@gmail.com>
2022-02-02 10:11:55 +0200

    (future) removed as not being used

--- woi/src/future/mod.rs
+++
@@ -1,2 +0,0 @@
-mod ready;
-pub(crate) use ready::{ok, Ready};
--- woi/src/lib.rs
+++ woi/src/lib.rs
@@ -1,7 +1,8 @@
-pub mod future;
 pub mod io;
 pub mod macros;
 pub mod net;
 pub mod task;
 pub mod runtime;
+pub mod channel;
+
 pub use runtime::Runtime;
