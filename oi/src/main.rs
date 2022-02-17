6d429e1d7527151056423ace79188b7d69457af6
SenYeezus <simpsonsenyo@gmail.com>
2022-02-17 14:16:33 +0200

    (oi) toy example with join! macro

--- oi/src/main.rs
+++ oi/src/main.rs
@@ -6,22 +6,57 @@ use woi;
 use woi::channel::mpsc;
 use woi::io::AsyncReadExt;
 use woi::net::TcpStream;
-use woi::time::sleep;
 use woi::Runtime;
+use woi::time::sleep;
 
 enum Mains {
     Channel,
     TcpRead,
     Sleep,
+    Join
 }
 
 fn main() {
-    let m = Mains::Channel;
+    let m = Mains::Join;
     match m {
         Mains::Channel => channel(),
         Mains::TcpRead => tcp_read(),
         Mains::Sleep => go_sleep(),
+        Mains::Join => join()
     }
+}
+
+fn join() {
+    tracing_subscriber::fmt::init();
+
+    let rt = Runtime::new();
+    rt.block_on(async {
+        let (tx, rx) = mpsc::channel();
+
+        let h1 = woi::spawn(async {
+            let tx = tx.clone();
+            println!("Sending message from handle 1");
+            tx.send("hello").unwrap()
+        });
+
+        let h2 = woi::spawn(async move {
+            println!("Sending message from handle one after sleeping");
+            sleep(Duration::from_secs(1)).await;
+            println!("Done sleeping. Sending message from handle one");
+            tx.send("hello world").unwrap();
+            println!("Sent message!");
+        });
+
+        let h3 = woi::spawn(async move {
+            println!("Received message: {}", rx.recv().await.unwrap());
+            println!("Received message: {}", rx.recv().await.unwrap());
+        });
+
+        woi::join!(h1, h2, h3);
+    });
+
+    println!("Finished")
+
 }
 
 fn channel() {
