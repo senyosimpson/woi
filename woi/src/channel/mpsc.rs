87f793f2fcbc9c5309a24d75e05e07ab370c59d1
SenYeezus <simpsonsenyo@gmail.com>
2022-02-18 14:19:06 +0200

    (channel) implemented errors for mpsc (#16)

--- woi/src/channel/mpsc.rs
+++ woi/src/channel/mpsc.rs
@@ -10,16 +10,24 @@ use std::task::{Context, Poll, Waker};
 
 use futures::future::poll_fn;
 
+use super::error::{SendError, TryRecvError};
+
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let chan = Rc::new(Channel::new());
     (Sender::new(chan.clone()), Receiver::new(chan))
 }
 
+pub struct Sender<T> {
+    chan: Rc<Channel<T>>,
+}
+
+pub struct Receiver<T> {
+    chan: Rc<Channel<T>>,
+}
+
 pub struct Channel<T> {
     // Inner state of the channel
     inner: RefCell<Inner<T>>,
-    // Waker notified when items are pushed into the channel
-    // rx_waker: Option<Waker>,
 }
 
 struct Inner<T> {
@@ -34,18 +42,65 @@ struct Inner<T> {
     rx_waker: Option<Waker>,
 }
 
-pub struct Sender<T> {
-    chan: Rc<Channel<T>>,
-}
-
-pub struct Receiver<T> {
-    chan: Rc<Channel<T>>,
-}
-
 enum State {
     Open,
-    SendClosed,
-    RecvClosed,
+    Closed,
+}
+
+// ==== impl Sender =====
+
+impl<T> Sender<T> {
+    pub fn new(chan: Rc<Channel<T>>) -> Sender<T> {
+        Sender { chan }
+    }
+
+    // This does not need to be async as sending to an unbounded queue
+    // will never block
+    pub fn send(&self, message: T) -> Result<(), SendError<T>> {
+        self.chan.send(message)
+    }
+}
+
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        self.chan.incr_tx_count();
+        Self {
+            chan: self.chan.clone(),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        tracing::debug!("Dropping sender");
+        self.chan.decr_tx_count();
+        if self.chan.tx_count() == 0 {
+            self.chan.close();
+        }
+    }
+}
+
+// ===== impl Receiver =====
+
+impl<T> Receiver<T> {
+    pub fn new(chan: Rc<Channel<T>>) -> Receiver<T> {
+        Receiver { chan }
+    }
+
+    pub async fn recv(&self) -> Option<T> {
+        poll_fn(|cx| self.chan.recv(cx)).await
+    }
+
+    pub fn try_recv(&self) -> Result<T, TryRecvError> {
+        self.chan.try_recv()
+    }
+}
+
+impl<T> Drop for Receiver<T> {
+    fn drop(&mut self) {
+        tracing::debug!("Dropping receiver");
+        self.chan.close();
+    }
 }
 
 // ===== impl Channel =====
@@ -62,6 +117,7 @@ impl<T> Channel<T> {
         }
     }
 
+    #[allow(unused)]
     fn wake_rx(&self) {
         let mut inner = self.inner.borrow_mut();
         if let Some(waker) = inner.rx_waker.take() {
@@ -69,14 +125,9 @@ impl<T> Channel<T> {
         }
     }
 
-    fn close_send(&self) {
+    fn close(&self) {
         let mut inner = self.inner.borrow_mut();
-        inner.state = State::SendClosed
-    }
-
-    fn close_recv(&self) {
-        let mut inner = self.inner.borrow_mut();
-        inner.state = State::RecvClosed
+        inner.state = State::Closed;
     }
 
     fn incr_tx_count(&self) {
@@ -93,7 +144,7 @@ impl<T> Channel<T> {
         self.inner.borrow().tx_count
     }
 
-    pub fn send(&self, message: T) {
+    pub fn send(&self, message: T) -> Result<(), SendError<T>> {
         let mut inner = self.inner.borrow_mut();
         match inner.state {
             State::Open => {
@@ -101,15 +152,9 @@ impl<T> Channel<T> {
                 if let Some(rx_waker) = &inner.rx_waker {
                     rx_waker.wake_by_ref();
                 }
+                Ok(())
             }
-            // If either half is closed, we actually cannot send
-            // any data. In the case we've dropped all send handles,
-            // we want to keep the channel closed. In the case the
-            // receiving side has been dropped, we can't send any
-            // messages
-            State::SendClosed | State::RecvClosed => {
-                // return error
-            }
+            State::Closed => Err(SendError(message)),
         }
     }
 
@@ -124,8 +169,8 @@ impl<T> Channel<T> {
             // empty temporarily and could be populated in future. We
             // register the rx waker to be woken when a new task is pushed
             // into the channel.
-            // If either side of the channel is closed, then we know
-            // that no new messages are coming through and we return None
+            // If the channel is closed, then we know that no new messages
+            // are coming through and we return None
             None => {
                 match inner.state {
                     State::Open => {
@@ -139,62 +184,20 @@ impl<T> Channel<T> {
 
                         Poll::Pending
                     }
-                    State::SendClosed | State::RecvClosed => Poll::Ready(None),
+                    State::Closed => Poll::Ready(None),
                 }
             }
         }
     }
-}
 
-// ==== impl Sender =====
-
-impl<T> Sender<T> {
-    pub fn new(chan: Rc<Channel<T>>) -> Sender<T> {
-        Sender { chan }
-    }
-
-    // This does not need to be async as sending to an unbounded queue
-    // will never block
-    pub fn send(&self, message: T) -> Result<(), std::io::Error> {
-        self.chan.send(message);
-        Ok(())
-    }
-}
-
-impl<T> Clone for Sender<T> {
-    fn clone(&self) -> Self {
-        self.chan.incr_tx_count();
-        Self {
-            chan: self.chan.clone(),
+    pub fn try_recv(&self) -> Result<T, TryRecvError> {
+        let mut inner = self.inner.borrow_mut();
+        match inner.queue.pop_front() {
+            Some(message) => Ok(message),
+            None => match inner.state {
+                State::Open => Err(TryRecvError::Empty),
+                State::Closed => Err(TryRecvError::Disconnected),
+            },
         }
-    }
-}
-
-impl<T> Drop for Sender<T> {
-    fn drop(&mut self) {
-        self.chan.decr_tx_count();
-        if self.chan.tx_count() == 0 {
-            self.chan.close_send();
-        }
-
-        self.chan.wake_rx();
-    }
-}
-
-// ===== impl Receiver =====
-
-impl<T> Receiver<T> {
-    pub fn new(chan: Rc<Channel<T>>) -> Receiver<T> {
-        Receiver { chan }
-    }
-
-    pub async fn recv(&self) -> Option<T> {
-        poll_fn(|cx| self.chan.recv(cx)).await
-    }
-}
-
-impl<T> Drop for Receiver<T> {
-    fn drop(&mut self) {
-        self.chan.close_recv();
     }
 }
