7e7ef09dc5806152808d8e4979c48859ec971964
SenYeezus <simpsonsenyo@gmail.com>
2022-03-04 17:21:05 +0200

    updated privacy for exported crates and traits

--- woi/src/io/mod.rs
+++ woi/src/io/mod.rs
@@ -1,10 +1,8 @@
-mod epoll;
+pub(crate) mod epoll;
 pub(crate) mod io_source;
 pub(crate) mod pollable;
 pub(crate) mod reactor;
 pub(crate) mod readiness;
 
-// Re-export
-pub use futures::io::{
-    AsyncBufRead, AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncSeek, AsyncWrite, AsyncWriteExt,
-};
+pub use futures::io::{AsyncReadExt, AsyncWriteExt};
+pub(crate) use futures::io::{AsyncRead, AsyncWrite};
